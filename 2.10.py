d84fca498aca4cf2e123d660b7c8eb236bd740c6
edvard2201 <edvard.stepanyan11@inbox.ru>
2025-11-17 22:28:07 +0300

    Add files via upload

---
+++ 2.10.py
@@ -0,0 +1,7 @@
+num1 = int(input("2"))
+num2 = int(input("4"))
+average_arithmetic = (num1 + num2) / 2
+print(f"{average_arithmetic}")
+if num1 >= 0 and num2 >= 0:
+average_arithmetic = math.sqrt(num1 * num2)
+print(f"{average_geometric}")
